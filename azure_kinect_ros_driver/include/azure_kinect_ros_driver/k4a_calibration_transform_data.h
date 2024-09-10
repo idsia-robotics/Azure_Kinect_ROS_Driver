e11aecf1ec81e80745a9f3d26f58a8d95b1fe65f
Simone Arreghini <arreghini.simone@gmail.com>
2024-09-10 13:40:24 +0200

    Renamed frames with azure_kinect prefix

--- azure_kinect_ros_driver/include/azure_kinect_ros_driver/k4a_calibration_transform_data.h
+++ azure_kinect_ros_driver/include/azure_kinect_ros_driver/k4a_calibration_transform_data.h
@@ -49,10 +49,10 @@ public:
   k4a::image transformed_depth_image_;
 
   std::string tf_prefix_ = "";
-  std::string camera_base_frame_ = "camera_base";
-  std::string rgb_camera_frame_ = "rgb_camera_link";
-  std::string depth_camera_frame_ = "depth_camera_link";
-  std::string imu_frame_ = "imu_link";
+  std::string camera_base_frame_ = "azure_kinect_camera_base";
+  std::string rgb_camera_frame_ = "azure_kinect_rgb_camera_link";
+  std::string depth_camera_frame_ = "azure_kinect_depth_camera_link";
+  std::string imu_frame_ = "azure_kinect_imu_link";
 
 private:
   void initialize(const K4AROSDeviceParams& params);
