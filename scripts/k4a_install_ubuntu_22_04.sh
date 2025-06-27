8b939d194d130a936cbc119cd89293b42d0bfef6
arsimone <arreghini.simone@gmail.com>
2025-06-27 18:38:00 +0200

    typo in script

--- scripts/k4a_install_ubuntu_22_04.sh
+++ scripts/k4a_install_ubuntu_22_04.sh
@@ -1,8 +1,8 @@
-# sudo apt-add-repository -y -n 'deb http://archive.ubuntu.com/ubuntu focal main'
-# sudo apt-add-repository -y 'deb http://archive.ubuntu.com/ubuntu focal universe'
-# sudo apt-get install -y libsoundio1
-# sudo apt-add-repository -r -y -n 'deb http://archive.ubuntu.com/ubuntu focal universe'
-# sudo apt-add-repository -r -y 'deb http://archive.ubuntu.com/ubuntu focal main'
+sudo apt-add-repository -y -n 'deb http://archive.ubuntu.com/ubuntu focal main'
+sudo apt-add-repository -y 'deb http://archive.ubuntu.com/ubuntu focal universe'
+sudo apt-get install -y libsoundio1
+sudo apt-add-repository -r -y -n 'deb http://archive.ubuntu.com/ubuntu focal universe'
+sudo apt-add-repository -r -y 'deb http://archive.ubuntu.com/ubuntu focal main'
 
 sudo curl -sSL https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/libk/libk4a1.4/libk4a1.4_1.4.1_amd64.deb > /tmp/libk4a1.4_1.4.1_amd64.deb
 sudo curl -sSL https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/libk/libk4a1.4-dev/libk4a1.4-dev_1.4.1_amd64.deb > /tmp/libk4a1.4-dev_1.4.1_amd64.deb
